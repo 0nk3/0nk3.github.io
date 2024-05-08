97868be7e61153f9e155caba30a6a98dfd992744
0nk3 <53526231+0nk3@users.noreply.github.com>
2024-05-08 07:42:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,13 +25,14 @@ let alterStyles = (isBackToTopRendered) => {
     "scale(0)";
 };
 
-window.onload = function() {
+window.addEventListener('load', function() {
     var images = document.querySelectorAll('.float img');
     for (var i = 0; i < images.length; i++) {
         var delay = Math.random() * (7 - 1) + 1; 
         images[i].style.animationDelay = delay + 's';
     }
-};
+});
+
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 700) {
